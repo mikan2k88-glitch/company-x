00b973d10f75acce783c37ad3dfdcb247a0f8840
mikan2k88-glitch <mikan2k88@gmail.com>
2026-08-25 02:44:10 +0900

    Update scout_engine.py

--- core/scout_engine.py
+++ core/scout_engine.py
@@ -1,7 +1,7 @@
 """
 core/scout_engine.py
 --------------------
-市場機会・物理調査ニーズの自動検知モジュール
+市場スカウトエンジン
 """
 
 import logging
@@ -11,10 +11,13 @@ logger = logging.getLogger("company_x.scout")
 
 
 class ScoutEngine:
-    def find_opportunity(self) -> Dict[str, Any]:
-        """市場ニーズを自動探知（サンプルデータ）"""
+    def scout_market(self) -> Dict[str, Any]:
+        """
+        市場機会をスカウトしてタスク提案オブジェクトを返す
+        """
+        logger.info("市場スカウト実行: Shibuya Pedestrian Density Sampling 案件を特定")
         return {
             "task_name": "Shibuya Pedestrian Density Sampling",
-            "intent": "渋谷スクランブル交差点の歩行者流動データサンプリング",
-            "estimated_cost_jpy": 8000.0
+            "intent": "Execute physical pedestrian density measurement in Shibuya",
+            "estimated_cost_jpy": 7200.0
         }
